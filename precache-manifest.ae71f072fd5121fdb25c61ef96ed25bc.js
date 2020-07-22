81b4ce4272706437e04022ff8e4915d0d53a368b
JanisDeveloper <janis@developer.lv>
2020-07-22 13:26:27 +0300

    Updates

---
+++ precache-manifest.ae71f072fd5121fdb25c61ef96ed25bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03704e1ca923a44f255fc34ceec91743",
+    "url": "/carousel-with-intro-outro/index.html"
+  },
+  {
+    "revision": "c56f3ca2f4665baf394a",
+    "url": "/carousel-with-intro-outro/static/css/main.2fc2f8cd.chunk.css"
+  },
+  {
+    "revision": "d088cf070bf41e60bd8d",
+    "url": "/carousel-with-intro-outro/static/js/2.cee156d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/carousel-with-intro-outro/static/js/2.cee156d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56f3ca2f4665baf394a",
+    "url": "/carousel-with-intro-outro/static/js/main.3194717d.chunk.js"
+  },
+  {
+    "revision": "33a7b3389799ac506d49",
+    "url": "/carousel-with-intro-outro/static/js/runtime-main.8136bdf1.js"
+  }
+]);
